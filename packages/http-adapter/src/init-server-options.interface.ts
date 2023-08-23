c404966403996b8cc765d9fdfcea40f96ac5cb36
augusito <kaugustus15@gmail.com>
2023-08-23 15:22:30 +0300

    feat: add graceful option

--- packages/http-adapter/src/init-server-options.interface.ts
+++ packages/http-adapter/src/init-server-options.interface.ts
@@ -7,8 +7,19 @@ export interface InitServerOptions {
   tls?: TlsOptions;
 
   /**
+   * Whether server should support graceful shutdown, allowing active requests
+   * time to complete.
+   */
+  graceful?: boolean;
+
+  /**
    * The grace period: number of milliseconds to continue to try to finish
    * outstanding http server I/O before forcibly terminating it.
+   *
+   * Defaults to Infinity (don't force-close). If you want to immediately
+   * destroy all sockets set its value to `0`.
+   *
+   * See {@link https://www.npmjs.com/package/stoppable | stoppable}
    */
   grace?: number;
 }
