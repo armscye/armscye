c0c87525ee16e9b5fb6b6eaa44b9b5920ba55b27
augusito <kaugustus15@gmail.com>
2024-01-25 04:36:38 +0300

    refactor(core): Abstract<T> to Abstract<T = any>

--- packages/core/src/abstract.interface.ts
+++ packages/core/src/abstract.interface.ts
@@ -2,6 +2,6 @@
  * Represents an abstract class `T`, if applied to a concrete class it would stop being
  * instantiable.
  */
-export interface Abstract<T> extends Function {
+export interface Abstract<T = any> extends Function {
   prototype: T;
 }
